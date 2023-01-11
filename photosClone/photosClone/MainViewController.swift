44bd59ab0938ee4178716c6e1f1daa2af17444e7
AlpatyevNikita <chococacaococonut@gmail.com>
2023-01-12 00:52:47 +0300

    removed storyboard

---
+++ photosClone/photosClone/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  photosClone
+//
+//  Created by Nikita Alpatiev on 1/12/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
